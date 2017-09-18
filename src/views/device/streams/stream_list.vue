bf57464460acd4b8b39e480dba734fcc0a0ac7ba
Ans Riaz <ansriazch@gmail.com>
2017-09-18 11:10:24 +0200

    stream listing, editing, channels editing listing complete

--- src/views/device/streams/stream_list.vue
+++ src/views/device/streams/stream_list.vue
@@ -23,14 +23,15 @@
             <div class="clearfix" style="background-color: #f0f3f5; padding:10px;">
               <div>
                 <div class="text-left text-center" style="float: left;">
-                  <p style="text-align: center; font-weight:bold; margin:0;">{{stream.title}}</p>
+                  <!-- <p style="text-align: center; font-weight:bold; margin:0;">{{stream.title}}</p> -->
+                  <b-form-input v-model="stream.title"
+                  type="text" style="border: none !important; border-color: transparent !important; background-color: transparent !important; font-weight: bold;"
+                  placeholder="Enter Stream Name"></b-form-input>
                 </div>
                 <div class="col-md-2" style="float: right;">
                   <div style="float: right;" right>
                     <b-button class="btn btn-danger" data="stream" @click="(event) => { onDeleteStream(event, stream) }">Delete Stream</b-button>
                   </div>
-                  &nbsp;
-                  &nbsp;
                   <div style="float: right;" right>
                     <b-button class="btn btn-primary" data="stream" @click="(event) => { onCreateChannelButton(event, stream) }">Add Channel</b-button>
                   </div>
@@ -58,42 +59,29 @@
                   <!-- style="border: none !important; border-color: transparent !important;" -->
                 </div>
                 <div class="col-md-3 text-left" id="divChannelType" data="stream" data-ch="channel" style="padding: 10px">
-                  <b-form-select variant="outline-secondary" class="mr-3" @change.native="(event) => { onChangeOptionChannelType(event, stream, channel) }" v-model="channel.type" :options="channelTypes" />
-                  </div>
-                  <div class="col-md-3 text-left" id="divChannelUnit" data="stream" data-ch="channel" style="padding: 10px">
-                    <b-form-input v-model="channel.unit"
-                    type="text"
-                    placeholder="Enter Channel Unit"></b-form-input>
-                  </div>
-                  <div class="col-md-3 text-center" data="stream" data-ch="channel" right style="padding: 10px">
-                    <button class="btn btn-danger" type="button" @click="(event) => { onDeleteChannelButton(event, stream, channel) }">Delete</button>
-                  </div>
+                  <b-form-select variant="outline-secondary" class="mr-3" @change.native="(event) => { onChangeOptionChannelType(event, stream, channel) }" v-model="channel.type" :options="channelTypes"> </b-form-select>
+                </div>
+                <div class="col-md-3 text-left" id="divChannelUnit" data="stream" data-ch="channel" style="padding: 10px">
+                  <b-form-input v-model="channel.unit"
+                  type="text"
+                  placeholder="Enter Channel Unit"></b-form-input>
+                </div>
+                <div class="col-md-3 text-center" data="stream" data-ch="channel" right style="padding: 10px">
+                  <button class="btn btn-danger" type="button" @click="(event) => { onDeleteChannelButton(event, stream, channel) }">Delete</button>
                 </div>
               </div>
-              <div class="float-right" right>
-                <button class="btn btn-success" type="button" @click="(event) => { onSaveStreamButton(event, stream) }">Save</button>
-              </div>
-              <!-- <b-table show-empty :items="stream.channels" :fields="stream.fields"> -->
-            <!-- <template slot="description" scope="row">{{row.item.description}}</template>
-            <template slot="created" scope="row">{{formatDate(row.item.createdAt * 1000)}}</template>
-            <template slot="updated" scope="row">{{formatDate(row.item.updatedAt * 1000)}}</template>
-            <template slot="actions" scope="row">
-              <div class="row">
-                <b-button class="btn btn-sm" :to="{ name: 'Streams', params: { deviceId: row.item.id }}">Streams</b-button>
-                <b-button class="btn btn-primary btn-sm" :variant="primary" :to="{ name: 'RecordSet', params: { deviceId: row.item.id }}">Records</b-button>
-                <click-confirm>
-                  <b-button class="btn btn-outline-danger btn-sm" :variant="danger" @click="remove(row.item.id)">Delete</b-button>
-                </click-confirm>
-              </div>
-            </template> -->
-            <!-- </b-table> -->
+            </div>
           </b-card>
         </div>
-        <!-- <span v-html="streamDetails"></span> -->
+        <div class="float-right" right>
+          <button class="btn btn-success" type="button" @click="(event) => { onSaveStreamButton(event, stream) }">Save</button>
+        </div>
       </b-card>
     </div>
   </div>
 </template>
+
+
 
 <script>
   import moment from 'moment'
@@ -173,10 +161,6 @@
         }
         // console.log(this.tableDataStreams)
       },
-      onChangeOptionStream (evt) {
-        let val = evt.target.value;
-        console.log(val)
-      },
       remove (stream) {
         this.$log.debug("Deleting %s", stream)
         console.log(stream)
@@ -191,95 +175,94 @@
       },
       onChangeOptionChannelType (evt, stream, channel) {
         let val = evt.target.value;
-        // console.log(val)
-        let parent = evt.target.parentElement
-        // console.log(parent)
-        for (var i = 0; i < stream.channels.length; i++) {
-          if(channel.id == stream.channels[i].id){
-            stream.channels['type'] = val;
-            break
-          }
-        }
-        // for (var i = 0; i < this.streams.length; i++) {
-        //   if(this.streams[i].title == stream.title) {
-        //     this.streams = stream
+        // for (var i = 0; i < stream.channels.length; i++) {
+        //   if(channel.id == stream.channels[i].id){
+        //     stream.channels['type'] = val;
+        //     break
         //   }
         // }
+        for (var i = 0; i < this.tableDataStreams.length; i++) {
+          if(this.tableDataStreams[i].title == stream.title) {
+            let channels = this.tableDataStreams[i].channels
+            for (var j = 0; j < channels.length; j++) {
+              if(channel.id == channels[j].id){
+                channels[i]['type'] = val;
+                break
+              }
+            }
+          }
+        }
       },
       onCreateChannelButton (evt, stream) {
         let val = evt.target.value;
-        // console.log(stream)
-        // console.log(evt.target)
-        // for (var i = 0; i < stream.length; i++) {
-        //   if(stream[i].channels
-        // }
-        stream.channels.push({name: "",
-          type: this.channelTypes,
-          unit: ""})
-        // console.log(stream)
-        // for (var i = 0; i < this.streams.length; i++) {
-          // if(this.streams[stream.title]) {
-          //   this.streams[stream.title].channels[name] = {name: "",
-          //     type: this.channelTypes,
-          //     unit: ""}
-          // }
-        // }
-        // console.log(JSON.stringify(this.streams))
-        // let parent = evt.target.parentElement
-        // console.log(parent)
-        // console.log(parent.data)
+        let obj = {name: "",
+        type: this.channelTypes,
+        id: stream.channels.length,
+        unit: ""};
+        for (var i = 0; i < this.tableDataStreams.length; i++) {
+          if(this.tableDataStreams[i].title == stream.title) {
+            this.tableDataStreams[i].channels.push(obj)
+          }
+        }
       },
       onDeleteChannelButton (evt, stream, channel) {
-        // let val = evt.target.value;
-        // console.log("stream" + JSON.stringify(stream))
-        // console.log("channel" + JSON.stringify(channel))
-        for (var i = 0; i < stream.channels.length; i++) {
-          if(channel.id == stream.channels[i].id){
-            stream.channels.splice(i,1);
-            break
+        // for (var i = 0; i < stream.channels.length; i++) {
+        //   // console.log(channel.id + " => " + stream.channels[i].id)
+        //   if(channel.id == stream.channels[i].id){
+        //     stream.channels.splice(i,1);
+        //     // console.log("removed " + i)
+        //     break
+        //   }
+        // }
+        for (var i = 0; i < this.tableDataStreams.length; i++) {
+          if(this.tableDataStreams[i].title == stream.title) {
+            let channels = this.tableDataStreams[i].channels
+            for (var j = 0; j < channels.length; j++) {
+              if(channel.id == channels[j].id){
+                channels.splice(j,1);
+                break
+              }
+            }
+            // console.log(stream)
+            // this.tableDataStreams[i] = stream
           }
         }
-        for (var i = 0; i < this.streams.length; i++) {
-          if(this.streams[i].title == stream.title) {
-            this.streams = stream
-          }
-        }
-        // let parent = evt.target.parentElement
-        // console.log(parent)
-        // console.log(parent.data)
       },
       onSaveStreamButton (evt, stream) {
         let json = this.device.toJSON()
         // console.log(stream)
         let updatedStream = {}
-        let keys = Object.keys(this.streams)
-        for (var i = 0; i < keys.length; i++) {
-          console.log(keys[i])
-          if(this.streams[stream.title] && keys[i] == stream.title) {
-            console.log(this.streams[stream.title])
-            let obj = {}
-            for (var j = 0; j < stream.channels.length; j++) {
-              obj[stream.channels[j].name] = stream.channels[j].type
-            }
-            updatedStream[stream.title] = obj
-          } else {
-            console.log(this.streams[keys[i]])
-            let obj = {}
-            let stChannels = this.streams[keys[i]].channels
-            let chKeys = Object.keys(stChannels)
-            console.log(chKeys)
-            console.log(stChannels)
-            for (var j = 0; j < chKeys.length; j++) {
-              obj[stChannels[chKeys[j]].name] = stChannels[chKeys[j]].type
-            }
-            updatedStream[keys[i]] = obj
-          }
-        }
+        // let keys = Object.keys(this.streams)
+        // for (var i = 0; i < keys.length; i++) {
+        //   console.log(keys[i])
+        //   if(this.streams[stream.title] && keys[i] == stream.title) {
+        //     console.log(this.streams[stream.title])
+        //     let obj = {}
+        //     for (var j = 0; j < stream.channels.length; j++) {
+        //       obj[stream.channels[j].name] = stream.channels[j].type
+        //     }
+        //     updatedStream[stream.title] = obj
+        //   } else {
+        //     console.log(this.streams[keys[i]])
+        //     let obj = {}
+        //     let stChannels = this.streams[keys[i]].channels
+        //     let chKeys = Object.keys(stChannels)
+        //     console.log(chKeys)
+        //     console.log(stChannels)
+        //     for (var j = 0; j < chKeys.length; j++) {
+        //       obj[stChannels[chKeys[j]].name] = stChannels[chKeys[j]].type
+        //     }
+        //     updatedStream[keys[i]] = obj
+        //   }
+        // }
         // add new strem
         for (var i = 0; i < this.tableDataStreams.length; i++) {
-          if(this.tableDataStreams[i].title == updatedStream[]){
-            
+          let st = this.tableDataStreams[i]
+          let obj = {}
+          for (var j = 0; j < st.channels.length; j++) {
+            obj[st.channels[j].name] = st.channels[j].type
           }
+          updatedStream[st.title] = obj
         }
         console.log("=============================updated stream=============================")
         // console.log(updatedStream)
@@ -292,7 +275,7 @@
         })
         console.log(json)
         console.log("=============================final stored stream=============================")
-        // return this.$raptor.Inventory().update(json)
+        return this.$raptor.Inventory().update(json)
       },
       onDeleteStream (evt, stream) {
         for (var i = 0; i < this.tableDataStreams.length; i++) {
@@ -304,12 +287,12 @@
       },
       onAddStreamButton (evt) {
         var st = {
-          channels: {
+          channels: [{
             name: "",
             type: this.channelTypes,
             unit: ""
-          },
-          title: "new stream"
+          }],
+          title: ""
         }
         // console.log(JSON.stringify(st))
         this.tableDataStreams.push(st)
