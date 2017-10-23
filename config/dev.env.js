b7685e4751c2a2eabf90161b9f06eb289e499e1c
Ans Riaz <ansriazch@gmail.com>
2017-10-23 18:03:51 +0200

    firebase added as database for user widgets

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,13 +1,11 @@
 var merge = require('webpack-merge')
 var prodEnv = require('./prod.env')
 
-var url = 'http://raptor.local'
-var urlPro = 'http://api.raptorbox.eu'
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   RAPTOR: `{
-      "url": "http://raptorbox.ddns.net",
-      "username": "memosa",
-      "password": "memosa.EIT2017",
+      "url": "http://raptor.local",
+      "username": "",
+      "password": "",
   }`
 })
